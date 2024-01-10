86015435840450f74d81529a627bc9819939cfd1
akbarsazish <sazishaliakbar@gmail.com>
2024-01-10 07:42:30 +0330

    complet authenctiation of user

--- src/components/generalComponent/Header.jsx
+++ src/components/generalComponent/Header.jsx
@@ -36,28 +36,18 @@ const Header = () => {
         <div className='relative flex h-16 items-center justify-between'>
           <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
             {/* Mobile menu button*/}
-            <button
-              type='button'
+            <button type='button'
               className='relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'
-              aria-controls='mobile-menu'
-              aria-expanded='false'
-            >
+              aria-controls='mobile-menu' aria-expanded='false'>
+
               <span className='absolute -inset-0.5'></span>
               <span className='sr-only'>Open main menu</span>
 
-              <svg
-                className='block h-6 w-6'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth='1.5'
-                stroke='currentColor'
-                aria-hidden='true'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
-                />
+              <svg className='block h-6 w-6'
+                fill='none' viewBox='0 0 24 24'
+                strokeWidth='1.5' stroke='currentColor'
+                aria-hidden='true' >
+                <path strokeLinecap='round' strokeLinejoin='round' d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'/>
               </svg>
 
               <svg
