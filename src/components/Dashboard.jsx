b8241ea3f297ad90535d143f31451ce06781eba4
akbarsazish <sazishaliakbar@gmail.com>
2024-01-12 22:54:24 +0330

    fetch news api

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,55 +1,49 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
-
 
 export function Dashboard () {
   const {params} = useParams();
   const [news, setNews] = useState('')
 
-
-const options = {
-  method: 'GET',
-  url: 'https://google-news-api1.p.rapidapi.com/search',
-  params: {
-    language: '<REQUIRED>'
-  },
-  headers: {
-    'X-RapidAPI-Key': '137494cab9msh33a37a92275f6ebp15d6bfjsn893894bf8f50',
-    'X-RapidAPI-Host': 'google-news-api1.p.rapidapi.com'
+  
+  async function fetchData() {
+      let url = 'https://content.guardianapis.com/search?q=debate&tag=politics/politics&from-date=2014-01-01&api-key=a186bd39-09ff-432c-a22b-1c69cb8f295c';
+      try {
+          const response = await axios.get(url);
+          console.log(response.data);
+      } catch (error) {
+          console.error('Error:', error);
+      }
   }
-};
+  
+  fetchData();
+  
 
-try {
-	const response =  axios.request(options);
-	console.log("new api ", response.data);
-} catch (error) {
-	console.error(error);
-}
+  
 
 
-  const apiKey = '45dd2940771a41ec90568a20e58c4244'
-  const url = `https://newsapi.org/v2/everything?q=Apple&from=2024-01-05&sortBy=popularity&apiKey=${apiKey}`
+//   const apiKey = '45dd2940771a41ec90568a20e58c4244'
+//   const url = `https://newsapi.org/v2/everything?q=Apple&from=2024-01-05&sortBy=popularity&apiKey=${apiKey}`
 
- useEffect(()=>{
-  axios.get(url).then((respons)=>{
-    setNews(respons.data.articles)
-  })
+//  useEffect(()=>{
+//   axios.get(url).then((respons)=>{
+//     setNews(respons.data.articles)
+//   })
 
- }, [])
+//  }, [])
 
- useEffect(()=>{
-  axios.post('http://192.168.10.21:8000/api/createNews', {news}).then((respons)=>{
-  }).catch((error)=>{
-    console.log(error)
-  })
+//  useEffect(()=>{
+//   axios.post('http://192.168.10.21:8000/api/createNews', {news}).then((respons)=>{
+//   }).catch((error)=>{
+//     console.log(error)
+//   })
 
- }, [])
+//  }, [])
  
- console.log(news)
   return (
     <>
-   
+     <h1> Hello </h1>
     </>
   )
 }
